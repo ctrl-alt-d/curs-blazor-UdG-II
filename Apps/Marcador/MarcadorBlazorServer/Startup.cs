8fd22d64adee60eb09ea782ef1863315b17bb9b3
dani <ctrl.alt.d@gmail.com>
2021-04-27 08:13:59 +0200

    progressar app marcador

--- Apps/Marcador/MarcadorBlazorServer/Startup.cs
+++ Apps/Marcador/MarcadorBlazorServer/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarcadorBackendDelFrontEnd.Abstracts;
+using MarcadorBackendDelFrontEnd.InMemory;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +30,9 @@ namespace MarcadorBlazorServer
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddSingleton<IMarcadorMachine, MarcadorMachine>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
