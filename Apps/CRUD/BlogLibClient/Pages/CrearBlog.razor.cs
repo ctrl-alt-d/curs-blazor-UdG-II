8580efb86aa9b173dc867b16e6eb48c4d0de1ddf
dani herrera <dani.herrera@udg.cat>
2021-04-29 15:53:19 +0200

    WIP. Creat component input bootstrap

---
+++ Apps/CRUD/BlogLibClient/Pages/CrearBlog.razor.cs
@@ -0,0 +1,42 @@
+﻿using Blogging.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogging.Client.Components.Pages
+{
+    public partial class CrearBlog
+    {
+
+        private CrearBlogWebView MyViewModel = new();
+
+        [Parameter] public int Id { get; set; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+            }
+        }
+
+        private async Task Submit()
+        {
+
+
+            var parms = new BlogCreateParms(
+                nomBlog: MyViewModel.Nom,
+                titolPrimerPost: MyViewModel.NomPost,
+                contingutPerAdults: MyViewModel.ContingutPerAdults
+                );
+            var resultat = await _BL.CreateBlog(parms);
+
+            _NM.NavigateTo("/fetchdata");
+
+        }
+
+    }
+}
---
+++ Apps/CRUD/BlogLibClient/Pages/CrearBlogWebView.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blogging.Client.Components.Pages
+{
+    public partial class CrearBlog
+    {
+        public class CrearBlogWebView
+        {
+            [Required(ErrorMessage = "El camp nom és obligatori")]
+            public string Nom { get; set; }
+
+            [Required(ErrorMessage = "El camp titol primer post és obligatori")]
+            public string NomPost { get; internal set; }
+            public bool ContingutPerAdults { get; internal set; }
+        }
+
+    }
+}
