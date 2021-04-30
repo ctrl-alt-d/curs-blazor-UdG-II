dafd10dbf8a2f33638a5fe1a54d164177492ef2d
dani herrera <dani.herrera@udg.cat>
2021-04-30 10:26:42 +0200

    Sessió 2

--- Apps/CRUD/BlogLibClient/Pages/CrearBlog.razor.cs
+++ Apps/CRUD/BlogLibClient/Pages/CrearBlog.razor.cs
@@ -1,9 +1,5 @@
 ﻿using Blogging.Shared;
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Blogging.Client.Components.Pages
