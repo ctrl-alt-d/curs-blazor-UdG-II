e7ce66e772c6e42801478fa404e59d8db15396e0
dani herrera <dani.herrera@udg.cat>
2021-05-05 12:58:22 +0200

    Afegir demo integracio bootstrap

---
+++ Apps/BS/BS/BS/Shared/BSAlert.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BS.Shared
+{
+    public partial class BSAlert
+    {
+
+        protected virtual List<string> LlistaDeMissatges { get; set; } = new();
+        public void MostraAixo(string msg)
+        {
+            LlistaDeMissatges.Add(msg);
+            StateHasChanged();
+
+            //
+            var aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += (_, _) =>
+            {
+                LlistaDeMissatges.Remove(msg);
+                StateHasChanged();
+            };
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+        }
+    }
+}
---
+++ Apps/BS/BS/BS/Shared/BSTable.razor.cs
@@ -0,0 +1,18 @@
+﻿using BS.Pages;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BS.Shared
+{
+    public partial class BSTable<T>
+    {
+        [Parameter] public IEnumerable<T> datasource { get; set; } = Array.Empty<T>();
+
+        [Parameter] public RenderFragment Head { get; set; }
+
+        [Parameter] public RenderFragment<T> Body { get; set; }
+    }
+}
